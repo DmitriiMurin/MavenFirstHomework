9936585852a189abe136c9d5f38b7897c8e91da4
DmitriiMurin <murin.dima@gmail.com>
2023-04-14 14:59:20 +0300

    Initial commit

---
+++ service/src/main/java/ru/madika/service/MyService.java
@@ -0,0 +1,28 @@
+package ru.madika.service;
+
+import ru.madika.db.Db;
+import ru.madika.db.DbSetting;
+import ru.madika.db.MyEntity;
+
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
